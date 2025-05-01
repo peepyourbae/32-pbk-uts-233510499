8b89b7cbb5fb945c95b2d6fb98c208a0ee8cda50
peepyourbae <afifalfani739@gmail.com>
2025-05-01 09:13:47 +0700

    input task and display

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,35 @@
 <script setup>
+import { ref } from 'vue';
+
+const items = ref([])
+const newItem = ref('')
+
+const addItem = () => {
+  const text = newItem.value.trim()
+  if (text) {
+    items.value.push({
+      id: Date.now(),
+      text,
+      completed: false
+    })
+    newItem.value = ''
+  }
+}
 </script>
 
 <template>
-  
+  <h1>To-Do List</h1>
+  <input v-model="newItem" type="text" @keyup.enter="addItem" placeholder="Add a new item">
+  <button @click="addItem">Add</button>
+
+
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.text }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
